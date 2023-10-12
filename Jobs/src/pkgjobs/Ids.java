f80a95280accc9aa99a90e40f0195dbfbbc3e92b
Aaron Scott <147019715+AaronScott2025@users.noreply.github.com>
2023-10-12 01:18:51 -0400

    Ids.java

--- Jobs/src/pkgjobs/Ids.java
+++ Jobs/src/pkgjobs/Ids.java
@@ -7,9 +7,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
+/**
+* Ids class. Used for all things related to importing the Employee information into the txt file.
+* Includes an employee number generator, and checks for duplicate ID's (All id's begin with 'W').
+*/
+
 public class Ids {
 	
-	
+	/**
+	 * Takes the information from Data Class, and creates an ID using the idNumGen() Method below
+  	 * Appends the information to the file, in a comma seperated list.
+         * Returns the id number(used in JobGui)
+	 */
 	public String id(Data d) throws IOException {
 		String name = d.getName();
 		String dept = d.getDept();
@@ -25,7 +34,12 @@ public class Ids {
 		
 		return id;
 	}
-	
+	/**
+	 * Employee ID generator.
+  	 * Each ID is 5 numbers, with a W in front of them
+    	 * The loop serves the purpose of checking for duplicates within a seperate ID list.
+      	 * If no duplicates are found, the new ID is placed into the idNumbers file, and the id is returned to the method above.
+	 */
 	public String idNumGen() throws IOException {
 		String id = "W";
 		Random gen = new Random();
