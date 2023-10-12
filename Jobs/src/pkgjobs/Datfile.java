603359524337b8c4c17009b0f362e6cbd36d8f94
Aaron Scott <147019715+AaronScott2025@users.noreply.github.com>
2023-10-12 01:10:06 -0400

    Datfile.java

--- Jobs/src/pkgjobs/Datfile.java
+++ Jobs/src/pkgjobs/Datfile.java
@@ -4,23 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+* Datfile class, uses BufferedReader to locate the key(Employee ID)
+* Returns the Id, along with all of the employee information if found
+* If not found, Returns an error message
+*/
+
 public class Datfile {
 	
 	
 	public String finder(String key) throws IOException {
 		String line;
-	
 		BufferedReader search = new BufferedReader(new FileReader("id.txt"));
 		while ((line = search.readLine()) != null) {
-			String curr = line;
-			String id = line.substring(0,6);
-			System.out.println(id);
-			if(key.equalsIgnoreCase(id)) {
-				return curr;
+			String curr = line; //Current Full Line
+			String id = line.substring(0,6); //Just the ID
+			if(key.equalsIgnoreCase(id)) { //IgnoreCase doesnt check for Deep Copy
+				return curr; //Found ID
 			}
 			
 	}
-		return "Cannot find employee";
+		return "Cannot find employee"; //No ID found
 
 	}
 }
